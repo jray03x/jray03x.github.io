8063a3a20486c1dc0f4389567eb84b22c9169225
JRI <95413551+jray03x@users.noreply.github.com>
2024-04-02 10:08:40 +0800

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,11 +1,14 @@
-        function openNav() {
-            document.getElementById("mySidenav").style.width = "150px";
-            document.querySelector(".main").style.marginLeft = "100px"; /* Push main content to the right */
-        }
-        function closeNav() {
-            document.getElementById("mySidenav").style.width = "0";
-            document.querySelector(".main").style.marginLeft= "0"; /* Bring main content back to center */
-        }
+function openNav() {
+    document.getElementById("mySidenav").style.width = "150px";
+    document.querySelector(".main").style.marginLeft = "150px"; /* Push main content to the right */
+    document.querySelector(".navbar").style.marginLeft = "150px"; /* Push navbar to the right */
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.querySelector(".main").style.marginLeft= "0"; /* Bring main content back to center */
+    document.querySelector(".navbar").style.marginLeft= "0"; /* Bring navbar back to center */
+}
         function getRandomFact() {
             // Fetch a new random fact from an API
             fetch('https://uselessfacts.jsph.pl/random.json?language=en')
