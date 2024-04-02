2d0e86921c39bd63c5545c8d6cc25e37f619df8d
JRI <95413551+jray03x@users.noreply.github.com>
2024-04-02 09:15:31 +0800

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,16 @@
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "150px";
+            document.querySelector(".main").style.marginLeft = "100px"; /* Push main content to the right */
+        }
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+            document.querySelector(".main").style.marginLeft= "0"; /* Bring main content back to center */
+        }
+        function getRandomFact() {
+            // Fetch a new random fact from an API
+            fetch('https://uselessfacts.jsph.pl/random.json?language=en')
+                .then(response => response.json())
+                .then(data => {
+                    document.getElementById('randomFact').textContent = data.text;
+                });
+        }
